e61729c2bab71cf0a78b8c99d2522872445e1fd8
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-08-11 12:33:28 -0700

    fix just arch

--- files/scripts/manuallyinstalljust.sh
+++ files/scripts/manuallyinstalljust.sh
@@ -16,7 +16,8 @@ set -oue pipefail
 # See the License for the specific language governing permissions and limitations under the License.
 LATEST_URL="$(curl -Ls -o /dev/null -w '%{url_effective}' https://github.com/casey/just/releases/latest)"
 VER="$(basename "$LATEST_URL")"
-curl -fLs --create-dirs "https://github.com/casey/just/releases/download/${VER}/just-${VER}-x86_64-unknown-linux-musl.tar.gz" -o "/tmp/just-${VER}-x86_64-unknown-linux-musl.tar.gz"
+ARCH="$(arch)"
+curl -fLs --create-dirs "https://github.com/casey/just/releases/download/${VER}/just-${VER}-${ARCH}-unknown-linux-musl.tar.gz" -o "/tmp/just-${VER}-x86_64-unknown-linux-musl.tar.gz"
 curl -fLs --create-dirs "https://github.com/casey/just/releases/download/${VER}/SHA256SUMS" -o /tmp/SHA256SUMS
 cd /tmp
 if ! sha256sum -c SHA256SUMS --ignore-missing
@@ -28,5 +29,5 @@ cd -
 mkdir -p /tmp/just && tar -xzf "/tmp/just-${VER}-x86_64-unknown-linux-musl.tar.gz" -C /tmp/just/
 cp /tmp/just/just /usr/bin/just && chmod 0755 /usr/bin/just
 cp /tmp/just/completions/just.bash /usr/share/bash-completion/completions/just
-rm "/tmp/just-${VER}-x86_64-unknown-linux-musl.tar.gz"
+rm "/tmp/just-${VER}-${ARCH}-unknown-linux-musl.tar.gz"
 rm -r /tmp/just/
