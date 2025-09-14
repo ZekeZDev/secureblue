a820f884103825309bd2ac411c4777d3447cd859
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-09-14 09:11:35 -0700

    temp: comment replacements

--- files/scripts/installproprietarypackages.sh
+++ files/scripts/installproprietarypackages.sh
@@ -20,18 +20,21 @@ rpm-ostree install libopenjph
 curl -Lo /etc/yum.repos.d/negativo17-fedora-multimedia.repo https://negativo17.org/repos/fedora-multimedia.repo
 sed -i '/^enabled=1/a\priority=90' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 
-rpm-ostree override replace \
-  --experimental \
-  --from repo='fedora-multimedia' \
-    libheif \
-    libva \
-    mesa-dri-drivers \
-    mesa-filesystem \
-    mesa-libEGL \
-    mesa-libGL \
-    mesa-libgbm \
-    mesa-libxatracker \
-    mesa-va-drivers \
-    mesa-vulkan-drivers \
-    gstreamer1-plugin-libav \
-    rar
+# rpm-ostree override replace \
+#   --experimental \
+#   --from repo='fedora-multimedia' \
+#     libheif \
+#     libva \
+#     libva-intel-media-driver \
+#     intel-gmmlib \
+#     intel-mediasdk \
+#     mesa-dri-drivers \
+#     mesa-filesystem \
+#     mesa-libEGL \
+#     mesa-libGL \
+#     mesa-libgbm \
+#     mesa-libxatracker \
+#     mesa-va-drivers \
+#     mesa-vulkan-drivers \
+#     gstreamer1-plugin-libav \
+#     rar
