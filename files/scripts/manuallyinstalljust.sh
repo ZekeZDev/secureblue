ca02204cce03c92da3e9420d3d6085dc540b5ff1
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-09-21 17:29:17 -0700

    fixes

--- files/scripts/manuallyinstalljust.sh
+++ files/scripts/manuallyinstalljust.sh
@@ -1,22 +1,8 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-# Copyright 2025 fiftydinar
-# Copyright 2025 The Secureblue Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software distributed under the License is
-# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 LATEST_URL="$(curl -Ls -o /dev/null -w '%{url_effective}' https://github.com/casey/just/releases/latest)"
 VER="$(basename "$LATEST_URL")"
-curl -fLs --create-dirs "https://github.com/casey/just/releases/download/${VER}/just-${VER}-x86_64-unknown-linux-musl.tar.gz" -o "/tmp/just-${VER}-x86_64-unknown-linux-musl.tar.gz"
+ARCH="$(arch)"
+curl -fLs --create-dirs "https://github.com/casey/just/releases/download/${VER}/just-${VER}-${ARCH}-unknown-linux-musl.tar.gz" -o "/tmp/just-${VER}-${ARCH}-unknown-linux-musl.tar.gz"
 curl -fLs --create-dirs "https://github.com/casey/just/releases/download/${VER}/SHA256SUMS" -o /tmp/SHA256SUMS
 cd /tmp
 if ! sha256sum -c SHA256SUMS --ignore-missing
@@ -25,9 +11,9 @@ then
     exit 1
 fi
 cd -
-mkdir -p /tmp/just && tar -xzf "/tmp/just-${VER}-x86_64-unknown-linux-musl.tar.gz" -C /tmp/just/
+mkdir -p /tmp/just && tar -xzf "/tmp/just-${VER}-${ARCH}-unknown-linux-musl.tar.gz" -C /tmp/just/
 cp /tmp/just/just /usr/bin/just && chmod 0755 /usr/bin/just
 cp /tmp/just/completions/just.bash /usr/share/bash-completion/completions/just
 cp /tmp/just/just.1 /usr/share/man/man1/just.1
-rm "/tmp/just-${VER}-x86_64-unknown-linux-musl.tar.gz"
+rm "/tmp/just-${VER}-${ARCH}-unknown-linux-musl.tar.gz"
 rm -r /tmp/just/
